3e077aaf1b5d39c21f1fe579675ad284936c0d50
trawwa <43407282+trawwa@users.noreply.github.com>
2021-08-03 17:59:12 +0700

    Continued coding

--- ##generator/main.py
+++ ##generator/main.py
@@ -11,3 +11,24 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("#Tag Generator")
 
         os.chdir(os.path.dirname(os.path.realpath(__file__)))
+        self.setWindowIcon(QtGui.QIcon("icon.ico"))
+
+        self.front_element = QtGui.QFont()
+        self.front_element.setPointSize(16)
+        self.front_element.setBold(True)
+        self.front_element.setWeight(75)
+
+        self.long_tags = QtGui.QFont()
+        self.long_tags.setPointSize(12)
+        self.long_tags.setBold(True)
+        self.long_tags.setWeight(75)
+
+        self.initUI()
+
+        self.tag_list = self.read_tags()
+        self.lcd_tagCount.display(int(len(self.tag_list)))
+
+    def initUI(self):
+        self.button_add = QtWidgets.QPushButton(self)
+        self.button_add.setObjectName("button_add")
+        self.button_add.setGeometry(QtCore.QRect(10, 10, 111, 81))
